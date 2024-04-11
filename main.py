ccc1c9c499d376625fd6a5737c048214b523af7d
vrushali <vchaudhari20comp@student.mes.ac.in>
2024-04-11 16:50:05 +0530

    Add data transformation pipeline to main.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from src.price_prediction import logger
 from src.price_prediction.pipeline.stage_data_ingestion import DataIngestionTrainingPipeline
 from src.price_prediction.pipeline.stage_data_validation import DataValidationTrainingPipeline
-
+from src.price_prediction.pipeline.stage_data_transformation import DataTransformationTrainingPipeline
 
 data_ingestion_pipeline = DataIngestionTrainingPipeline()
 data_ingestion_pipeline.main()
 data_validation_pipeline = DataValidationTrainingPipeline()
 data_validation_pipeline.main()
+data_transformation_pipeline = DataTransformationTrainingPipeline()
+data_transformation_pipeline.main()
