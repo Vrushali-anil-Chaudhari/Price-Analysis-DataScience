b253c28c6aabd01a3e32f4d16ecbd7fcea816485
vrushali <vchaudhari20comp@student.mes.ac.in>
2024-04-11 16:50:30 +0530

    Add DataTransformationConfig to Configuration class

--- src/price_prediction/config/configuration.py
+++ src/price_prediction/config/configuration.py
@@ -1,6 +1,6 @@
 from src.price_prediction.constants import *
 from src.price_prediction.utils.common import read_yaml, create_directories
-from price_prediction.entity.config_entity import DataIngestionConfig, DataValidationConfig
+from price_prediction.entity.config_entity import DataIngestionConfig, DataValidationConfig,DataTransformationConfig
 class Configuration:
     def __init__(self, 
     config_filepath = CONFIG_FILE_PATH,
@@ -40,3 +40,13 @@ class Configuration:
         )
 
         return data_validation_config
+    
+    def get_data_transformation_config(self) -> DataTransformationConfig:
+        config = self.config.data_transformation
+
+        create_directories([config.root_dir])
+        data_transformation_config = DataTransformationConfig(
+            root_dir = Path(config.root_dir),
+            data_path = Path(config.data_path)
+        )
+        return data_transformation_config
