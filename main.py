cdd5aff5a9dbe404f0829abe1d5dd6c292fcea1d
vrushali <vchaudhari20comp@student.mes.ac.in>
2024-04-11 21:04:17 +0530

    Add model training pipeline to main.py

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ from src.price_prediction import logger
 from src.price_prediction.pipeline.stage_data_ingestion import DataIngestionTrainingPipeline
 from src.price_prediction.pipeline.stage_data_validation import DataValidationTrainingPipeline
 from src.price_prediction.pipeline.stage_data_transformation import DataTransformationTrainingPipeline
-
+from src.price_prediction.pipeline.stage_model_training import ModelTrainingPipeline
 data_ingestion_pipeline = DataIngestionTrainingPipeline()
 data_ingestion_pipeline.main()
 data_validation_pipeline = DataValidationTrainingPipeline()
 data_validation_pipeline.main()
 data_transformation_pipeline = DataTransformationTrainingPipeline()
 data_transformation_pipeline.main()
+model_training_pipeline = ModelTrainingPipeline()
+model_training_pipeline.main()
