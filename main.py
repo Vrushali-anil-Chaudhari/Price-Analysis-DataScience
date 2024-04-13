6158a1aa6523fceb62baf0b0d6373ed2cba33795
vrushali <vchaudhari20comp@student.mes.ac.in>
2024-04-13 12:45:54 +0530

    Add model evaluation pipeline to main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from src.price_prediction.pipeline.stage_data_ingestion import DataIngestionTrai
 from src.price_prediction.pipeline.stage_data_validation import DataValidationTrainingPipeline
 from src.price_prediction.pipeline.stage_data_transformation import DataTransformationTrainingPipeline
 from src.price_prediction.pipeline.stage_model_training import ModelTrainingPipeline
+from src.price_prediction.pipeline.stage_model_evaluation import ModelEvaluationPipeline
 data_ingestion_pipeline = DataIngestionTrainingPipeline()
 data_ingestion_pipeline.main()
 data_validation_pipeline = DataValidationTrainingPipeline()
@@ -11,3 +12,5 @@ data_transformation_pipeline = DataTransformationTrainingPipeline()
 data_transformation_pipeline.main()
 model_training_pipeline = ModelTrainingPipeline()
 model_training_pipeline.main()
+model_evaluation_pipeline = ModelEvaluationPipeline()
+model_evaluation_pipeline.main()
